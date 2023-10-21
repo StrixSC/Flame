aed2de6d67a76d5d4d5badedda2f962cbf20b86e
StrixSC <talon.strix@gmail.com>
2023-10-21 16:04:43 -0400

    feat: Added daisyu

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,9 @@
 function App(): JSX.Element {
   return (
     <div className="container">
-      <h1 className="text-3xl font-bold underline text-red-400">
-        Hello world!
-      </h1>
+      <div className="App">
+        <button className="btn">Hello daisyUI</button>
+      </div>
     </div>
   )
 }
